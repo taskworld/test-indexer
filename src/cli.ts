531b297528074125fef25a8845bfdd8fc5034ce9
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-10-04 17:58:41 +0700

    :sparkles: Indexing works!

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,25 @@
 import minimist from 'minimist'
 import { processTestResults } from './processTestResults'
 import { checkEnv } from './environment'
+import { writeFileSync } from 'fs'
+import Elasticsearch from 'elasticsearch'
+import AWS from 'aws-sdk'
+import { logger } from './logger'
 
 const args = minimist(process.argv.slice(2), {
-  boolean: ['process'],
+  boolean: ['index'],
+  string: ['save'],
 })
 
 async function main() {
-  console.log('Processing test results...')
+  logger.info('Processing test results...')
   const getArg = (name: string) => {
     if (args[name] == null) {
       throw new Error(`Required command line argument: --${name}`)
     }
     return String(args[name])
   }
-  const dataToIndex = await processTestResults({
+  const result = await processTestResults({
     testResultsDirs: args._,
     project: getArg('project'),
     category: getArg('category'),
@@ -22,8 +27,47 @@ async function main() {
     commit: getArg('commit'),
     branch: getArg('branch'),
   })
-  console.log(dataToIndex)
-  // const env = await checkEnv()
+  if (args.save) {
+    logger.info('Saving processing results to', args.save)
+    writeFileSync(args.save, JSON.stringify(result, null, 2))
+  }
+  if (args.index) {
+    logger.info('Going to index the result...')
+    const env = await checkEnv()
+    const region = env.endpoint.match(/([^.]+)\.es\.amazonaws\.com/)![1]
+    AWS.config.update({
+      credentials: new AWS.Credentials(env.accessKeyID, env.secretAccessKey),
+      region: region,
+    })
+    const es = new Elasticsearch.Client({
+      hosts: [env.endpoint],
+      connectionClass: require('http-aws-es'),
+    })
+    for (const indexName of Object.keys(result.index)) {
+      const docs = result.index[indexName]
+      const docIds = Object.keys(docs)
+      const chunkSize = 100
+      for (let i = 0; i < docIds.length; i += chunkSize) {
+        const docIdsThisChunk = docIds.slice(i, i + chunkSize)
+        logger.info(
+          { indexName },
+          'Indexing %d docs (%d/%d)',
+          docIdsThisChunk.length,
+          i + docIdsThisChunk.length,
+          docIds.length
+        )
+        const body: any[] = []
+        for (const docId of Object.keys(docIdsThisChunk)) {
+          body.push(
+            { index: { _index: indexName, _id: docId, _type: '_doc' } },
+            docs[docId]
+          )
+        }
+        await es.bulk({ body })
+      }
+    }
+  }
+  logger.info('* Operation completed successfully')
 }
 
 process.on('unhandledRejection', e => {
--- src/processTestResults.ts
+++ src/processTestResults.ts
@@ -1,4 +1,6 @@
 import { execFileSync } from 'child_process'
+import glob from 'glob'
+import { readFileSync } from 'fs'
 
 export type ProcessOptions = {
   testResultsDirs: string[]
@@ -19,7 +21,18 @@ export type ProcessOptions = {
   branch: string
 }
 
-export async function processTestResults(options: ProcessOptions) {
+export type ProcessResult = {
+  /** Data to be indexed to Elasticsearch */
+  index: {
+    [indexName: string]: {
+      [docId: string]: any
+    }
+  }
+}
+
+export async function processTestResults(
+  options: ProcessOptions
+): Promise<ProcessResult> {
   const allureCLI = require.resolve('allure-commandline/dist/bin/allure')
   if (!options.testResultsDirs.length) {
     throw new Error(
@@ -29,4 +42,21 @@ export async function processTestResults(options: ProcessOptions) {
   execFileSync(allureCLI, ['generate', '--clean', ...options.testResultsDirs], {
     stdio: 'inherit',
   })
+  const testcases = glob.sync('allure-report/data/test-cases/*.json')
+  const result: ProcessResult = {
+    index: { testcases: {} },
+  }
+  for (const filename of testcases) {
+    try {
+      const data = JSON.parse(readFileSync(filename, 'utf8'))
+      result.index.testcases[data.uid] = data
+    } catch (e) {
+      console.error(
+        'processTestResults(): Cannot process test case file',
+        filename,
+        e
+      )
+    }
+  }
+  return result
 }
